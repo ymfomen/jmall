ad50d2c89b9156da33be74335b7c99fb039c3cac
OMEN <ymfomen@qq.com>
2019-04-10 10:27:17 +0800

    分布式架构模块初始化

---
+++ mall-usermanage/src/main/java/com/ymfomen/jmall/mallusermanage/MallUsermanageApplication.java
@@ -0,0 +1,13 @@
+package com.ymfomen.jmall.mallusermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUsermanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUsermanageApplication.class, args);
+    }
+
+}
---
+++ mall-usermanage/src/test/java/com/ymfomen/jmall/mallusermanage/MallUsermanageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ymfomen.jmall.mallusermanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallUsermanageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
